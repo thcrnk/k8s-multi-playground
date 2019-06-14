19bf1c9a9d0ada8a81f1c6fa2ffaa98931082524
thcrnk <thcrnk@gmail.com>
2019-06-14 15:10:38 +0300

    changes to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 docker build -t thcrnk/multi-client:latest -t thcrnk/multi-client$SHA -f ./client/Dockerfile ./client
 docker build -t thcrnk/multi-server:latest -t thcrnk/multi-server$SHA -f ./server/Dockerfile ./server
-docker build -t thcrnk/multi-worker:latest -t thcrnk/multi-worker$SHA  -f ./worker/Dockerfile ./worker
+docker build -t thcrnk/multi-worker:latest -t thcrnk/multi-worker$SHA -f ./worker/Dockerfile ./worker
+
 docker push thcrnk/multi-client:latest
 docker push thcrnk/multi-server:latest
 docker push thcrnk/multi-worker:latest
@@ -12,5 +13,5 @@ docker push thcrnk/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=thcrnk/multi-server:$SHA
-kubectl set image deployments/client-deployment server=thcrnk/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=thcrnk/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=thcrnk/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=thcrnk/multi-worker:$SHA
